9fd8bb2ecefdcd676422158d4dd6ec243920108d
Gabriel Klein <gabriel.klein@philips.com>
2022-08-24 18:15:16 -0300

    vinculo ao BD e rotas node

---
+++ src/routes/checklist.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log('olá');
+    res.send();
+});
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(req.body);
+});
+
+router.get('/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send(`ID: ${req.params.id}`);
+});
+
+router.put('/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send(`PUT ID: ${req.params.id}`);
+});
+
+module.exports = router;
