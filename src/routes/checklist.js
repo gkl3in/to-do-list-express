7487347136c551123bd4f4bf62319485699b4fdb
Gabriel Klein <gabriel.klein@philips.com>
2022-08-25 18:23:40 -0300

    atualização do mongoose

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,24 +1,57 @@
 const express = require('express');
 
 const router = express.Router();
+const Checklist = require('../models/checklist');
 
-router.get('/', (req, res) => {
-    console.log('olá');
-    res.send();
+router.get('/', async (req, res) => {
+    try {
+        let checklists = await Checklist.find({});
+        res.status(200).json(checklists);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body);
+router.post('/', async (req, res) => {
+    let { name } = req.body;
+
+    try {
+        let checklists = await Checklist.create({ name });
+        res.status(200).json(checklists);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+
 });
 
-router.get('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`ID: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 });
 
-router.put('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`PUT ID: ${req.params.id}`);
+router.put('/:id', async (req, res) => {
+    let { name } = req.body;
+
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    let { name } = req.body;
+
+    try {
+        let checklist = await Checklist.findByIdAndRemove(req.params.id);
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 });
 
 module.exports = router;
